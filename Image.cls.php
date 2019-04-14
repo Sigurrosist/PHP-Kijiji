14b89653ffdab0bf3f5661bd8add32469c11f4bc
scami <scami@DESKTOP-BB7SKUS.wireless.concordia.ca>
2019-04-14 19:02:20 -0400

    Classes

---
+++ Image.cls.php
@@ -0,0 +1,54 @@
+<?php
+
+class Image 
+{
+   private $imageId;
+   private $imageSource;
+   
+   function __construct($imageSource)
+   {
+       $this->teacherId = self::$sequence++;
+       $this->imageSource = $imageSource;
+       
+   }
+   
+/**
+     * @return mixed
+     */
+    public function getImageId()
+    {
+        return $this->imageId;
+    }
+
+/**
+     * @return mixed
+     */
+    public function getImageSource()
+    {
+        return $this->imageSource;
+    }
+
+/**
+     * @param mixed $imageId
+     */
+    public function setImageId($imageId)
+    {
+        $this->imageId = $imageId;
+    }
+
+/**
+     * @param mixed $imageSource
+     */
+    public function setImageSource($imageSource)
+    {
+        $this->imageSource = $imageSource;
+    }
+
+   
+   
+}
+
+
+
+
+?>
