2b3d73600e0dcea75db5578ac7b144bf88ebb473
Judy <mokjooyeon00@gmail.com>
2019-04-15 14:16:05 -0400

    NavBar complete

--- index.php
+++ index.php
@@ -12,6 +12,28 @@
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"></span>
 </button>
+
+<div class="d-flex justify-content-end">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Register</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Post</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">EN / FR</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Log In</a>
+      </li>
+      <li class="nav-item dropdown">
+    </ul>
+</div>
 </nav>
 
 
